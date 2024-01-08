94954fff509774008f449333475844c3dd49bab8
carlosefrain123 <carlosefrainchallo1@gmail.com>
2024-01-08 11:06:10 -0500

    08/01/2024 - 11:05 - Se agrega 17 usuarios en la carpeta seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,28 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'name'=>'administrador',
+            'email'=>'administrador@gmail.com',
+            'address'=>'Av. Siempre Viva',
+            'phone'=>'09999999',
+            'password'=>Hash::make('12345678')
+        ]);
+        User::create([
+            'name'=>'doctoruno',
+            'email'=>'doctoruno@gmail.com',
+            'address'=>'Av. Siempre Viva',
+            'phone'=>'09999999',
+            'password'=>Hash::make('12345678')
+        ]);
+        User::create([
+            'name'=>'pacienteuno',
+            'email'=>'pacienteuno@gmail.com',
+            'address'=>'Av. Siempre Viva',
+            'phone'=>'09999999',
+            'password'=>Hash::make('12345678')
+        ]);
+        /**TODO: Creamos 17 registros falsos */
+        User::factory(17)->create();
     }
 }
