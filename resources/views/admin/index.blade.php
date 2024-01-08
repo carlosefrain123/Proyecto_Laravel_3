b0c4e7c3ad33df64ec38484db332e37d57059592
carlosefrain123 <carlosefrainchallo1@gmail.com>
2024-01-08 16:03:06 -0500

    08/01/2024 - 16:02 - Se agrega la carpeta admin - y en la carpeta controllers el AdminController.php

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,20 @@
+{{-- TODO: Se copia lo que está en la documentación "https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Usage" --}}
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
