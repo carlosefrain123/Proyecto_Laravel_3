97ced4fa3489eccb0f6071b8e046b3d3fef88171
carlosefrain123 <carlosefrainchallo1@gmail.com>
2024-01-08 22:47:54 -0500

    08/01/2024 - 22:45 - Se agrega más campos en el register.blade.php - en el guestblade.php se agrega el logo de Limpio Be - El RegisterUserController.php se agrega más campos y validaciones

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,12 +32,16 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
+            'address' => ['required', 'string', 'max:255'],
+            'phone' => ['numeric', 'digits:9'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
+            'address' => $request->address,
+            'phone' => $request->phone,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,7 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <img src="{{ asset('img/logoLimpioBe.png') }}" width="150px" height="150px">
                 </a>
             </div>
 
