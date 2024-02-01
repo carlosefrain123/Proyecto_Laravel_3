353caba6d5da7184572279e0c2e5bf2ac9368c2f
carlosefrain123 <carlosefrainchallo1@gmail.com>
2024-01-31 21:38:08 -0500

    31/01/2024 - 21:37 - Se agrega el user_id y speciality_id - Cabe recalcar que estamos haciendo relación de muchos a muchos

--- database/migrations/2024_01_31_213018_create_speciality_user_table.php
+++ database/migrations/2024_01_31_213018_create_speciality_user_table.php
@@ -15,7 +15,13 @@ return new class extends Migration
     {
         Schema::create('speciality_user', function (Blueprint $table) {
             $table->id();
-            
+            //TODO: Se crea las llaves foraneas
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            //
+            $table->unsignedBigInteger('speciality_id');
+            $table->foreign('speciality_id')->references('id')->on('specialities');
+            //todo: Luego de eso ejecutamos el comando de la migración: php artisan migrate
             $table->timestamps();
         });
     }
